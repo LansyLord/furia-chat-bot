8c7457d37b4350088b9fdceb2df905362c5447e9
lansylord <lansylord@gmail.com>
2025-05-04 18:45:24 -0300

    Preparando para deploy no Vercel

--- furia-chat-bot-front/src/app/services/chat.service.ts
+++ furia-chat-bot-front/src/app/services/chat.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Match } from '../models/match';
 import { Player } from '../models/player';
+import { environment } from '../../enviroments/enviroment.prod';
 
 interface ApiResponse {
   resposta: Match[]; // Usando o nome correto da propriedade
@@ -13,7 +14,7 @@ interface ApiResponse {
   providedIn: 'root'
 })
 export class ChatService {
-  private apiUrl = 'http://localhost:8080/api/v1';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
@@ -23,22 +24,22 @@ export class ChatService {
 
   // No seu ChatService do Angular
   getProximasPartidas(): Observable<Match[]> {
-    return this.http.get<Match[]>(`${this.apiUrl}/partidas/proximas`);
+    return this.http.get<Match[]>(`${this.apiUrl}api/v1/partidas/proximas`);
   }
 
   getUltimasPartidas(): Observable<Match[]> {
-    return this.http.get<Match[]>(`${this.apiUrl}/partidas/ultimas`)
+    return this.http.get<Match[]>(`${this.apiUrl}api/v1/partidas/ultimas`)
   }
 
   getJogadores(): Observable<Player[]> {
-    return this.http.get<Player[]>(`${this.apiUrl}/jogadores`);
+    return this.http.get<Player[]>(`${this.apiUrl}api/v1/jogadores`);
   }
 
   subscribeToNextMatchEmail(email: string) {
-    return this.http.post(`${this.apiUrl}/notificacoes/proxima-partida`, { email });
+    return this.http.post(`${this.apiUrl}api/v1/notificacoes/proxima-partida`, { email });
   }
 
   checkEmailExists(email: string): Observable<boolean> {
-    return this.http.get<boolean>(`${this.apiUrl}/check-email?email=${encodeURIComponent(email)}`);
+    return this.http.get<boolean>(`${this.apiUrl}api/v1/check-email?email=${encodeURIComponent(email)}`);
   }
 }
---
+++ furia-chat-bot-front/src/enviroments/enviroment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://furia-chat-bot-h4b6.onrender.com', // URL do seu backend no Render
+};
